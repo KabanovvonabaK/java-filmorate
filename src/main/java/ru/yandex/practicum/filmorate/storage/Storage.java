fae49c97732e3ae17d2444e21ea09f2f6cd9ab18
Pavel Kabanov <pkabanov.d@yandex.ru>
2023-04-01 07:59:53 +0300

    fix: small checkstyle fix v4 (if after all these years)

--- src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
@@ -3,12 +3,11 @@ package ru.yandex.practicum.filmorate.storage;
 import java.util.List;
 
 public interface Storage<T> {
-
     T create(T entity);
 
     T update(T entity);
 
     T findById(int id);
-    
+
     List<T> findAll();
 }
